76cdacdbf22f85e0a2b77eea2d6bba103c32f92e
mikedev101 <mikierules109@gmail.com>
2024-01-09 21:18:29 -0500

    Add README and setup SQL
    ^

---
+++ backend/README.md
@@ -0,0 +1,23 @@
+# Backend
+CloudLink Omega utilizes net/http for it's API. 
+
+## Basics
+All requests are either `text/plain` or `application/json`.
+Use this directory to build the server binary (use `go build .`).
+Configuration is done via an environment variables file. See `.env.example`
+for a template.
+
+## Database
+This backend code was designed with a MariaDB server in mind.
+
+Before starting the server, you will need to generate the CL Omega
+tables and procedures in the DB. The commands needed to generate
+these are in `init.sql`.
+
+Afterwards, point to a MariaDB server in an `.env` file (see `.env.example`).
+
+## Documentation
+This API utilizes Swagger to generate documentation.
+You will need to prepare a local install of Swagger before building `./docs`.
+Once Swagger is installed, navigate to this directory and run `swag init`.
+Documentation will be built and present on `http://localhost:8080/docs`.
---
+++ backend/init.sql
@@ -0,0 +1,246 @@
+-- Adminer 4.8.1 MySQL 11.2.2-MariaDB-1:11.2.2+maria~deb12 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+
+CREATE DATABASE `clomega` /*!40100 DEFAULT CHARACTER SET utf16 COLLATE utf16_unicode_ci */;
+USE `clomega`;
+
+DELIMITER ;;
+
+DROP FUNCTION IF EXISTS `createUser`;;
+CREATE FUNCTION `createUser`(`usernameParam` tinytext, `passwordParam` tinytext, `gamertagParam` tinytext, `emailParam` tinytext) RETURNS char(26) CHARSET utf16 COLLATE utf16_unicode_ci
+BEGIN
+    DECLARE userUlid CHAR(26);
+
+    -- Check if the email is unique
+    IF (SELECT COUNT(*) FROM users WHERE email = emailParam) != 0 THEN
+       SIGNAL SQLSTATE '45000'
+       SET MESSAGE_TEXT = 'EMAIL_IN_USE';
+    END IF;
+
+    -- Check if the username is unique
+    IF (SELECT COUNT(*) FROM users WHERE username = usernameParam) != 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USERNAME_TAKEN';
+    END IF;
+
+    -- Set user ULID
+    SET userUlid = ULID_FROM_DATETIME(NOW());
+
+    -- If both username and email are unique, insert the new user
+    INSERT INTO users (id, username, password, gamertag, email, created)
+    VALUES (userUlid, usernameParam, passwordParam, gamertagParam, emailParam, NOW());
+
+    RETURN 'OK';
+END;;
+
+DROP FUNCTION IF EXISTS `ULID_DECODE`;;
+CREATE FUNCTION `ULID_DECODE`(s CHAR(26)) RETURNS binary(16)
+    DETERMINISTIC
+BEGIN
+DECLARE s_base32 CHAR(26);
+SET s_base32 = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(UPPER(s), 'J', 'I'), 'K', 'J'), 'M', 'K'), 'N', 'L'), 'P', 'M'), 'Q', 'N'), 'R', 'O'), 'S', 'P'), 'T', 'Q'), 'V', 'R'), 'W', 'S'), 'X', 'T'), 'Y', 'U'), 'Z', 'V');
+RETURN UNHEX(CONCAT(LPAD(CONV(SUBSTRING(s_base32, 1, 2), 32, 16), 2, '0'), LPAD(CONV(SUBSTRING(s_base32, 3, 12), 32, 16), 15, '0'), LPAD(CONV(SUBSTRING(s_base32, 15, 12), 32, 16), 15, '0')));
+END;;
+
+DROP FUNCTION IF EXISTS `ULID_ENCODE`;;
+CREATE FUNCTION `ULID_ENCODE`(b BINARY(16)) RETURNS char(26) CHARSET utf16 COLLATE utf16_unicode_ci
+    DETERMINISTIC
+BEGIN
+DECLARE s_hex CHAR(32);
+SET s_hex = LPAD(HEX(b), 32, '0');
+RETURN REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(CONCAT(LPAD(CONV(SUBSTRING(s_hex, 1, 2), 16, 32), 2, '0'), LPAD(CONV(SUBSTRING(s_hex, 3, 15), 16, 32), 12, '0'), LPAD(CONV(SUBSTRING(s_hex, 18, 15), 16, 32), 12, '0')), 'V', 'Z'), 'U', 'Y'), 'T', 'X'), 'S', 'W'), 'R', 'V'), 'Q', 'T'), 'P', 'S'), 'O', 'R'), 'N', 'Q'), 'M', 'P'), 'L', 'N'), 'K', 'M'), 'J', 'K'), 'I', 'J');
+END;;
+
+DROP FUNCTION IF EXISTS `ULID_FROM_DATETIME`;;
+CREATE FUNCTION `ULID_FROM_DATETIME`(`t` datetime) RETURNS char(26) CHARSET utf16 COLLATE utf16_unicode_ci
+    DETERMINISTIC
+BEGIN
+RETURN ULID_ENCODE(CONCAT(UNHEX(CONV(UNIX_TIMESTAMP(t) * 1000, 10, 16)), RANDOM_BYTES(10)));
+END;;
+
+DROP FUNCTION IF EXISTS `ULID_TO_DATETIME`;;
+CREATE FUNCTION `ULID_TO_DATETIME`(s CHAR(26)) RETURNS datetime
+    DETERMINISTIC
+BEGIN
+RETURN FROM_UNIXTIME(CONV(HEX(LEFT(ULID_DECODE(s), 6)), 16, 10) / 1000);
+END;;
+
+DROP PROCEDURE IF EXISTS `addAdmin`;;
+CREATE PROCEDURE `addAdmin`(IN `userUlid` char(26))
+BEGIN
+    -- Check if the user ULID exists
+    IF (SELECT COUNT(*) FROM users WHERE id = userUlid) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USER_ULID_NOTFOUND';
+    END IF;
+
+    IF (SELECT COUNT(*) FROM admins WHERE userid = userUlid) != 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'ADMIN_ALREADY_ADDED';
+    END IF;
+
+    -- Insert into admins
+    INSERT INTO admins (userid)
+    VALUES (userUlid);
+
+    SELECT 'OK' AS result;
+END;;
+
+DROP PROCEDURE IF EXISTS `addDeveloperMember`;;
+CREATE PROCEDURE `addDeveloperMember`(IN `developerUlidParam` char(26), IN `userUlidParam` char(26))
+BEGIN
+    DECLARE memberExists INT;
+    
+    -- Check if ULID is valid
+    IF (SELECT COUNT(*) FROM users WHERE id = userUlidParam) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USER_ULID_NOTFOUND';
+    END IF;
+
+    -- Get developer ID based on ULID
+    IF (SELECT COUNT(*) FROM developers WHERE id = developerUlidParam) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'DEVELOPER_ULID_NOTFOUND';
+    END IF;
+
+    -- Check if the user is already a member of that developer, if the member doesn't exist, add them and finish the procedure
+    IF (SELECT COUNT(*) FROM developers_members WHERE developerid = developerUlidParam AND userid = userUlidParam) = 0 THEN
+        INSERT INTO developers_members (developerid, userid)
+        VALUES (developerUlidParam, userUlidParam);
+        SELECT 'OK' AS result;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USER_ALREADY_MEMBER';
+    END IF;
+END;;
+
+DROP PROCEDURE IF EXISTS `createDeveloper`;;
+CREATE PROCEDURE `createDeveloper`(IN `nameParam` tinytext, OUT `ulidParam` char(26))
+BEGIN
+    -- Create developer entry
+    SET ulidParam = ULID_FROM_DATETIME(NOW());
+    INSERT INTO developers (id, name)
+    VALUES (ulidParam, nameParam);
+
+    SELECT 'OK' AS result;
+END;;
+
+DROP PROCEDURE IF EXISTS `createSession`;;
+CREATE PROCEDURE `createSession`(IN `userUlid` char(26), IN `originParam` tinytext)
+BEGIN
+    DECLARE sessionUlid CHAR(26);
+
+    -- Get user ID based on email and return a message if not found
+    IF (SELECT COUNT(*) FROM users WHERE id = userUlid) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USER_NOTFOUND';
+    ELSE
+        -- Generate a ULID for the session token
+        SET sessionUlid = ULID_FROM_DATETIME(NOW());
+
+        -- Insert the session with current timestamp and expiry 24 hours from now
+        INSERT INTO sessions (id, userid, created, expires, origin)
+        VALUES (sessionUlid, userUlid, CURRENT_TIMESTAMP(), TIMESTAMPADD(HOUR, 24, CURRENT_TIMESTAMP()), originParam);
+
+        SELECT sessionUlid AS result;
+    END IF;
+END;;
+
+DROP PROCEDURE IF EXISTS `deleteDeveloper`;;
+CREATE PROCEDURE `deleteDeveloper`(IN `developerUlidParam` char(26))
+BEGIN
+    -- Verify if the developer UUID exists
+    IF (SELECT COUNT(*) FROM developers WHERE id = developerUlidParam) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'DEVELOPER_ULID_NOTFOUND';
+    END IF;
+
+    -- Remove members from developer_members
+    DELETE FROM developers_members
+    WHERE developerid = developerUlidParam;
+
+    -- Delete the entry in developers
+    DELETE FROM developers
+    WHERE id = developerUlidParam;
+
+    SELECT 'OK' AS result;
+END;;
+
+DROP PROCEDURE IF EXISTS `getUserPassword`;;
+CREATE PROCEDURE `getUserPassword`(IN `userUlid` char(26))
+BEGIN
+    -- Check if the user exists
+    IF (SELECT COUNT(*) FROM users WHERE id = userUlid) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USER_NOTFOUND';
+    END IF;
+
+    -- Return salted hash
+    SELECT password AS result
+    FROM users
+    WHERE id = userUlid;
+END;;
+
+DROP PROCEDURE IF EXISTS `getUserULID`;;
+CREATE PROCEDURE `getUserULID`(IN `usernameParam` tinytext)
+BEGIN
+    DECLARE userUlid CHAR(26);
+
+    -- Get user UUID based on email
+    SELECT id INTO userUlid
+    FROM users
+    WHERE username = usernameParam;
+
+    -- If user is not found, return NULL
+    IF userUlid IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USER_NOTFOUND';
+    ELSE
+        SELECT userUlid AS ULID;
+    END IF;
+END;;
+
+DROP PROCEDURE IF EXISTS `removeAdmin`;;
+CREATE PROCEDURE `removeAdmin`(IN `userUlid` char(26))
+BEGIN
+
+    -- Check if user exists
+    IF (SELECT COUNT(*) FROM users WHERE id = userUlid) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USER_NOTFOUND';
+    END IF;
+ 
+    -- Check if the user is already an admin
+    IF (SELECT COUNT(*) FROM admins WHERE userid = userUlid) = 0 THEN
+       SIGNAL SQLSTATE '45000'
+       SET MESSAGE_TEXT = 'USER_NOTADMIN';
+    ELSE 
+       DELETE FROM admins WHERE userid = userUlid;
+       SELECT 'OK' AS result;
+    END IF;
+END;;
+
+DROP PROCEDURE IF EXISTS `updateUserPassword`;;
+CREATE PROCEDURE `updateUserPassword`(IN `userUlid` tinytext, IN `newPass` text)
+BEGIN
+    -- Check if the user exists
+    IF (SELECT COUNT(*) FROM users WHERE id = userUlid) = 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'USER_NOTFOUND';
+    END IF;
+
+    -- Update salted hash
+    UPDATE users
+    SET password = newPass
+    WHERE id = userUlid;
+
+    -- Return OK
+    SELECT 'OK' AS result;
+END;;
+
+DELIMITER ;
+
+-- 2024-01-10 02:15:51
