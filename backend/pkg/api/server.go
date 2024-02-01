69f3ae0e3f164add3b8ddf419e31b0a7f472d803
Mike J. Renaker / "MikeDEV <mike@meower.org>
2024-02-01 15:45:59 -0500

    Update init/server & implement context

--- backend/pkg/api/server.go
+++ backend/pkg/api/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -14,11 +15,23 @@ import (
 func RunServer(port int, mgr *dm.Manager) error {
 	r := chi.NewRouter()
 
+	// Init DB
+	mgr.InitDB()
+
 	// Add logging and recovery middeware
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
 	// TODO: implement custom CORS middleware
+
+	// Mount middleware to pass data manager into requests
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, "dm", mgr)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	})
 
 	// Mount v0 route
 	r.Mount("/api/v0", v0.Router)
--- backend/pkg/api/v0/init.go
+++ backend/pkg/api/v0/init.go
@@ -2,12 +2,18 @@ package v0
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/mikedev101/cloudlink-omega/backend/pkg/api/v0/routes"
+	routes "github.com/mikedev101/cloudlink-omega/backend/pkg/api/v0/routes"
+	dm "github.com/mikedev101/cloudlink-omega/backend/pkg/data"
 )
 
 // Create chi router
 var Router = chi.NewRouter()
 
+// Passthrough for data manager
+var DataManager *dm.Manager
+
 func init() {
+	// Mount routes
 	Router.Route("/", routes.RootRouter)
+	Router.Route("/signaling", routes.SignalingRouter)
 }
